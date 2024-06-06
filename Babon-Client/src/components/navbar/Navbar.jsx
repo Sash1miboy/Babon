f91fa665ffc4e284c4086dd91a262b5736586ed9
Endra Djati <endra.djati2004@gmail.com>
2024-06-06 16:46:53 +0700

    update to ver 0.6

--- Babon-Client/src/components/navbar/Navbar.jsx
+++ Babon-Client/src/components/navbar/Navbar.jsx
@@ -1,16 +1,25 @@
 import React, { useState } from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom'
 import "./Navbar.scss";
+import newRequest from '../../utils/newRequest';
 
 const Navbar = () => {
 
   const [open, setOpen] = useState(false)
 
-  const currentUser= {
-    id:1,
-    userName:"Kendrick Lamar",
-    isSeller:true
-  };
+  const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      await newRequest.post("/auth/logout");
+      localStorage.setItem("currentUser", null);
+      navigate("/")
+    } catch (err) {
+      console.log(err)
+    }
+  }
 
   return (
     <div className='navbar'>
@@ -45,12 +54,12 @@ const Navbar = () => {
               )}
             </div>
             <div className="account-button">
-              {!currentUser && <button className='login-button'>Login</button>}
-              {!currentUser && <button className='register-button'>Register</button>}
+              {!currentUser && <Link className='link' to="/login"><button className='login-button'>Login</button></Link>}
+              {!currentUser && <Link className='link' to="/register"><button className='register-button'>Register</button></Link>}
               {currentUser && (
                 <div className="user" onClick={()=>setOpen(!open)}>
-                  <img src="/img/kdot.png" alt="" />
-                  <span className='username'>{currentUser?.userName.split(" ")[0]}</span>
+                  <img src={currentUser.img || "img/Profile.svg"} alt="" />
+                  <span className='username'>{currentUser?.username.split(" ")[0]}</span>
                   {open && <div className="user-options">
                     {
                       currentUser?.isSeller && (
@@ -61,9 +70,9 @@ const Navbar = () => {
                       )
                     }
                     <Link to="/OrderList" className='link'>Orders</Link>
-                    {!currentUser && <Link to="" className='link'>Become a Seller</Link>}
+                    {!currentUser.isSeller && <Link to="" className='link'>Become a Seller</Link>}
                     <Link to="/profile" className='link'>Profile</Link>
-                    <Link to="/login" className='link'>Logout</Link>
+                    <Link onClick={handleLogout} className='link'>Logout</Link>
                   </div>}
                 </div>
               )}
--- Babon-Client/src/pages/register/Register.jsx
+++ Babon-Client/src/pages/register/Register.jsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "./Register.scss"
 import { Link } from 'react-router-dom'
+import newRequest from '../../utils/newRequest';
+import { useNavigate } from 'react-router-dom';
 
 const Register = () => {
+  const [user, setUser] = useState({
+    username: "",
+    email: "",
+    password: "",
+    phoneNumber: "",
+    dateOfBirth: "",
+    isSeller: false,
+  });
+
+  const navigate = useNavigate();
+
+  const handleChange = (e)=>{
+    setUser(prev=>{
+      return {...prev, [e.target.name]: e.target.value};
+    });
+  };
+  
+  const handleSeller = (e)=>{
+    setUser(prev=>{
+      return {...prev, isSeller: e.target.checked};
+    });
+  };
+
+  const handleSubmit = async (e)=>{
+    e.preventDefault()
+    try {
+      await newRequest.post("/auth/register", {
+        ...user
+      });
+      navigate("/login")
+    } catch (err) {
+      console.log(err)
+    }
+  };
+
   return (
     <div className='register'>
       <div className="container">
@@ -18,41 +55,43 @@ const Register = () => {
           <div className="left">
             <img src="/img/calling.png" alt="" />
           </div>
-          <div className="right">
+          <form className="right" onSubmit={handleSubmit}>
             <h1>Registration</h1>
             <div className="input-bar">
               <div className="username-bar">
                 <label htmlFor="">Username</label>
-                <input type="text" placeholder='Enter your username'/>
+                <input type="text" name='username' placeholder='Enter your username' onChange={handleChange}/>
               </div>
               <div className="email-bar">
                 <label htmlFor="">Email</label>
-                <input type="email" name="" id="" placeholder='Enter your email address'/>
+                <input type="email" name="email" placeholder='Enter your email address' onChange={handleChange}/>
               </div>
               <div className="password-bar">
                 <label htmlFor="">Password</label>
-                <input type="password" name="" id="" placeholder='Enter your password'/>
+                <input type="password" name="password" placeholder='Enter your password' onChange={handleChange}/>
               </div>
               <div className="mobile-date">
                 <div className="mobile-bar">
                   <label htmlFor="">Mobile Number</label>
-                  <input type="number" name="" id="" placeholder='Enter your phone number'/>
+                  <input type="number" name="phoneNumber" placeholder='Enter your phone number'onChange={handleChange}/>
                 </div>
                 <div className="date-bar">
                   <label htmlFor="">Date of Birth</label>
-                  <input type="date" name="" id="" />
+                  <input type="date" name="dateOfBirth" onChange={handleChange}/>
                 </div>
               </div>
+            </div>
+            <div className="become-seller">
+              <input type="checkbox" name="isSeller" onChange={handleSeller}/>
+              <span>I Want to Become a Seller</span>
             </div>
             <div className="check-box">
               <input type="checkbox" name="" id="" />
               <span>I agree to the terms of use and the privacy policy</span>
             </div>
-            <Link to="/" className='link'>
-              <button>Register</button>
-            </Link>
+            <button type="submit">Register</button>
             <span className="already-have-account">Already have an account? <Link to="/login" className='link'>Login here</Link></span>
-          </div>
+          </form>
         </div>
       </div>
     </div>
