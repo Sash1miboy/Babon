f3bae6efddd302759e953b824f23e7198f869cb0
Endra Djati <endra.djati2004@gmail.com>
2024-06-16 18:36:51 +0700

    updated service controller and route

--- Babon-Server/routes/service.route.js
+++ Babon-Server/routes/service.route.js
@@ -1,7 +1,17 @@
 import express from "express";
+import {
+    createService, 
+    deleteService, 
+    getService, 
+    getServices
+} from "../controllers/service.controller.js";
+import {verifyToken} from "../middleware/jwt.js";
 
 const router = express.Router();
 
-router.get("/test", )
+router.post("/", verifyToken, createService)
+router.post("/:id", verifyToken, deleteService)
+router.post("/single/:id", verifyToken, getService)
+router.post("/", verifyToken, getServices)
 
 export default router;
