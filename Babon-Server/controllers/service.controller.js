93f9ed17fb1cda85d64a20d2be4fe893202bb3f7
Endra Djati <endra.djati2004@gmail.com>
2024-06-16 18:36:51 +0700

    updated service controller and route

--- Babon-Server/controllers/service.controller.js
+++ Babon-Server/controllers/service.controller.js
@@ -1,3 +1,22 @@
-export const fn = (req, res)=>{
+import Service from "../models/service.model.js";
+import createError  from "../utils/createError.js";
 
-}
+
+export const createService = async (req, res, next)=>{
+    if(!req.isSeller) return next (createError(403, "Only sellers can create a service!"));
+
+    const newService = new Service({
+        userId: req.userId,
+        ...req.body,
+    });
+
+    try {
+        const savedService = await newService.save();
+        res.status(201).json(savedService);
+    } catch (err) {
+      next(err)  
+    }
+};
+export const deleteService = async (req, res, next)=>{};
+export const getService = async (req, res, next)=>{};
+export const getServices = async (req, res, next)=>{};
