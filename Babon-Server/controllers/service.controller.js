9fce33304d35d261a57b480c04f3035ee6607192
Sash1miBoy <endra.djati2004@gmail.com>
2024-06-17 12:17:28 +0700

    update to service controller and route

--- Babon-Server/controllers/service.controller.js
+++ Babon-Server/controllers/service.controller.js
@@ -1,3 +1,4 @@
+import serviceModel from "../models/service.model.js";
 import Service from "../models/service.model.js";
 import createError  from "../utils/createError.js";
 
@@ -14,9 +15,44 @@ export const createService = async (req, res, next)=>{
         const savedService = await newService.save();
         res.status(201).json(savedService);
     } catch (err) {
-      next(err)  
+      next(err);
     }
 };
-export const deleteService = async (req, res, next)=>{};
-export const getService = async (req, res, next)=>{};
-export const getServices = async (req, res, next)=>{};
+export const deleteService = async (req, res, next)=>{
+    try {
+        const service = await Service.findById(req.params.id);
+
+        if(service.userId !== req.userId) 
+            return next(createError(403, "You can delete only your service!"));
+
+        await Service.findByIdAndDelete(req.params.id);
+        res.status(200).send("Service has been deleted");
+    } catch (err) {
+        next(err);
+    }
+};
+export const getService = async (req, res, next)=>{
+    try {
+        const service = await Service.findById(req.params.id);
+        if(!service) next(createError(404, "Service Not Found"))
+        res.status(200).send(service);
+} catch (err) {
+    next(err);
+}
+};
+export const getServices = async (req, res, next)=>{
+    const q = req.query;
+    const filters = {
+        ...(q.userId && {userId: q.userId}),
+        ...(q.category && {category: q.category}),
+        ...((q.min || q.max) && {price: {...(q.min && {$gt: q.min}), ...(q.max && {$lt: q.max})},
+    }),
+        ...(q.search && {title: {$regex: q.search, $options: "i"}})
+    };
+    try {
+        const services = await Service.find(filters)
+        res.status(200).send(services);
+    } catch (err) {
+        next(err);
+    }
+};
