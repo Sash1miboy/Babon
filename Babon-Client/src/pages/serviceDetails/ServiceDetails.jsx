d0115ee2f185a1970f8b6ca4087aaceff2c67b74
Endra Djati <endra.djati2004@gmail.com>
2024-06-19 14:08:44 +0700

    add review, service, order for only backend

--- Babon-Client/src/pages/serviceDetails/ServiceDetails.jsx
+++ Babon-Client/src/pages/serviceDetails/ServiceDetails.jsx
@@ -4,6 +4,7 @@ import { Slider } from 'infinite-react-carousel'
 import { Link, useParams } from 'react-router-dom'
 import { useQuery } from '@tanstack/react-query'
 import newRequest from '../../utils/newRequest'
+import Reviews from '../../components/reviews/Reviews'
 
 const ServiceDetails = () => {
 
@@ -17,12 +18,15 @@ const ServiceDetails = () => {
       })
   })
 
+  const userId = data?.userId
+
   const { isLoading: isLoadingUser, error: errorUser, data: dataUser } = useQuery({
     queryKey: ['user'],
     queryFn: () =>
       newRequest.get(`/users/${data.userId}`).then((res) => {
         return res.data;
-      })
+      }),
+      enabled: !!userId,
   })
 
   return (
@@ -60,60 +64,7 @@ const ServiceDetails = () => {
               {data.desc}
             </p>
           </div>
-          <div className="reviews">
-            <h2>Reviews</h2>
-            <div className="item">
-              <div className="user">
-                <img src="/img/pink.png" alt="" className="user" />
-                <span>Pink Guy</span>
-              </div>
-              <div className="ratings">
-                <img src="/img/star.png" alt="" className="stars" />
-                <span>5</span>
-              </div>
-              <p>
-                Saya baru saja menggunakan layanan "Membersihkan Toilet Paling Bersih 1 Jagat" dan saya sangat terkesan dengan hasilnya! 
-                Tim yang datang sangat profesional dan ramah, mereka membawa semua peralatan dan produk pembersih yang diperlukan.
-                Toilet saya dibersihkan dengan sangat teliti, tidak ada satu pun sudut yang terlewat. 
-                Bahkan noda yang sulit dihilangkan sebelumnya kini lenyap tanpa jejak.
-              </p>
-              <hr />
-            </div>
-            <div className="item">
-              <div className="user">
-                <img src="/img/skibidi.png" alt="" className="user" />
-                <span>Skibidi Toilet</span>
-              </div>
-              <div className="ratings">
-                <img src="/img/star.png" alt="" className="stars" />
-                <span>5</span>
-              </div>
-              <p>
-                Saya baru saja menggunakan layanan "Membersihkan Toilet Paling Bersih 1 Jagat" dan saya sangat terkesan dengan hasilnya! 
-                Tim yang datang sangat profesional dan ramah, mereka membawa semua peralatan dan produk pembersih yang diperlukan.
-                Toilet saya dibersihkan dengan sangat teliti, tidak ada satu pun sudut yang terlewat. 
-                Bahkan noda yang sulit dihilangkan sebelumnya kini lenyap tanpa jejak.
-              </p>
-              <hr />
-            </div>
-            <div className="item">
-              <div className="user">
-                <img src="/img/silver.png" alt="" className="user" />
-                <span>Silver Surfer</span>
-              </div>
-              <div className="ratings">
-                <img src="/img/star.png" alt="" className="stars" />
-                <span>5</span>
-              </div>
-              <p>
-                Saya baru saja menggunakan layanan "Membersihkan Toilet Paling Bersih 1 Jagat" dan saya sangat terkesan dengan hasilnya! 
-                Tim yang datang sangat profesional dan ramah, mereka membawa semua peralatan dan produk pembersih yang diperlukan.
-                Toilet saya dibersihkan dengan sangat teliti, tidak ada satu pun sudut yang terlewat. 
-                Bahkan noda yang sulit dihilangkan sebelumnya kini lenyap tanpa jejak.
-              </p>
-              <hr />
-            </div>
-          </div>
+          <Reviews serviceId={id}/>
         </div>
         <div className="right">
           <div className="price">
--- Babon-Server/controllers/review.controller.js
+++ Babon-Server/controllers/review.controller.js
@@ -1,3 +1,49 @@
-export const fn = (req, res)=>{
+import createError from "../utils/createError.js";
+import Review from "../models/review.model.js"
+import Service from "../models/service.model.js"
 
+export const createReview = async (req, res, next)=>{
+    if(req.isSeller) 
+        return next(createError(403, "Sellers can't create a review!"))
+
+    const newReview = new Review ({
+        userId: req.userId,
+        serviceId: req.body.serviceId,
+        desc: req.body.desc,
+        star: req.body.star,
+    });
+
+    try {
+        const review = await Review.findOne({
+            serviceId: req.body.serviceId, 
+            userId: req.userId,
+        });
+
+        if(review) return next(createError(403, "You already created a reviews for this service!"));
+
+        const savedReview = await newReview.save();
+        await Service.findByIdAndUpdate(req.body.serviceId, {
+            $inc: {totalStars: req.body.star, starNumber: 1},
+        });
+        res.status(201).send(savedReview);
+    } catch (err) {
+        next(err);
+    }
+};
+
+export const getReviews = async (req, res, next)=>{
+    try {
+        const reviews = await Review.find({serviceId: req.params.serviceId})
+        res.status(200).send(reviews);
+    } catch (err) {
+        next(err);
+    }
+}
+
+export const deleteReview = async (req, res, next)=>{
+    try {
+        
+    } catch (err) {
+        next(err);
+    }
 }
