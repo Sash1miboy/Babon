f7370605dd9b57e027ca7a6a2bf56d7999c9c694
Endra Djati <endra.djati2004@gmail.com>
2024-06-20 09:47:39 +0700

    add stripe payment but still error

--- Babon-Server/controllers/order.controller.js
+++ Babon-Server/controllers/order.controller.js
@@ -1,28 +1,6 @@
-import Order from "../models/order.model.js"
-import Service from "../models/service.model.js"
-
-export const createOrder = async (req, res, next)=>{
-    try {
-
-        const service = await Service.findById(req.params.serviceId);
-
-        const newOrder = new Order({
-            serviceId: service._id,
-            img: service.coverImage,
-            title: service.title,
-            buyerId: req.userId,
-            sellerId: service.userId,
-            price: service.price,
-            payment_intent: "temporary",
-        });
-
-        await newOrder.save();
-        res.status(200).send("Successful!")
-        
-    } catch (err) {
-        next(err)
-    }
-}
+import Order from "../models/order.model.js";
+import Service from "../models/service.model.js";
+import Stripe from "stripe";
 
 export const getOrders = async (req, res, next)=>{
     try {
@@ -38,8 +16,51 @@ export const getOrders = async (req, res, next)=>{
 }
 
 export const intent = async (req, res, next)=>{
-    try {
+    const stripe = new Stripe(
+        process.env.STRIPE
+    );
 
+    const service = await Service.findById(req.params.id)
+
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: service.price * 100,
+        currency: "idr",
+        automatic_payment_methods: {
+          enabled: true,
+        },
+    });
+
+    const newOrder = new Order({
+        serviceId: service._id,
+        img: service.coverImage,
+        title: service.title,
+        buyerId: req.userId,
+        sellerId: service.userId,
+        price: service.price,
+        payment_intent: paymentIntent.id,
+    });
+
+    await newOrder.save();
+
+    res.status(200).send({
+        clientSecret: paymentIntent.client_secret,
+    })
+}
+
+export const confirm = async (req, res, next)=>{
+    try {
+        const orders = await Order.findOneAndUpdate(
+        {
+            payment_intent: req.body.paymentIntent
+        }, 
+        {
+            $set:{
+            isCompleted: true,
+            },
+        }
+    );
+
+        res.status(200).send("Order have been confirmed!");
     } catch (err) {
         next(err)
     }
